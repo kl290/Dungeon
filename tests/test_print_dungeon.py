e661fb919193d550887d3e5ba2f92cc74f397378
kl <kl@intentus.de>
2025-04-08 08:25:52 +0200

    OT-1986 - Test print_dungeon
    Test umstrukturiert:
    
    1 Testfall, 2 Dungeons mit unterschiedlicher Art und Belegung der Räume, am Ende sollen die richtigen print Werte erscheinen.
    
    2 Testfall, bezieht sich auf die Spieler Position und überprüft das bei einer Änderung der Position sich auch der Spieler (P) verschiebt.

--- tests/test_print_dungeon.py
+++ tests/test_print_dungeon.py
@@ -5,36 +5,85 @@ from unittest.mock import patch, call
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from Dungeon.generate_dungeon import generate_dungeon
 from Dungeon.print_dungeon import print_dungeon
 
 
 class TestPrintDungeon(unittest.TestCase):
 
-    @patch("builtins.print")
-    def test_eingang_position(self, mock_print):
-        dungeon = generate_dungeon(5, 5)
-        print_dungeon(dungeon, (-1, -1))
-        erstes_zeichen = mock_print.call_args_list[0][0][0]
-        self.assertEqual(erstes_zeichen, "E")
+    @patch('builtins.print')
+    def test_print_dungeon_rooms(self, mock_print):
+        tests = [
+            [
+                [
+                    [{'raumtyp': 'S', 'besucht': False}, {'raumtyp': 'F', 'besucht': True},
+                     {'raumtyp': 'F', 'besucht': True}],
+                    [{'raumtyp': 'F', 'besucht': True}, {'raumtyp': 'S', 'besucht': True},
+                     {'raumtyp': 'S', 'besucht': True}]
+                ],
+                [
+                    call('P', end = '  '), call('F', end = '  '), call('F', end = '  '),
+                    call(),
+                    call('F', end = '  '), call('S', end = '  '), call('S', end = '  '),
+                    call()
+                ]
+            ],
+            [
+                [
+                    [{'raumtyp': 'F', 'besucht': False}, {'raumtyp': 'S', 'besucht': True}],
+                    [{'raumtyp': 'L', 'besucht': True}, {'raumtyp': 'F', 'besucht': False}]
+                ],
+                [
+                    call('P', end = '  '),
+                    call('S', end = '  '),
+                    call(),
+                    call('L', end = '  '),
+                    call('?', end = '  '),
+                    call()
+                ]
+            ]
+        ]
+
+        for test in tests:
+            print_dungeon(test[0], {"x": 0, "y": 0})
+
+            self.assertEqual(mock_print.mock_calls, test[1])
+
+            mock_print.reset_mock()
 
     @patch('builtins.print')
-    def test_unbesuchte_raeume(self, mock_print):
-        dungeon = generate_dungeon(2, 1)
-        print_dungeon(dungeon, (-1, -1))
-        assert mock_print.mock_calls == [
-            call('E', end = '  '),
-            call(),
-            call('?', end = '  '),
-            call()]
+    def test_print_dungeon_spieler_position(self, mock_print):
+        tests = [
+            [
+                [
+                    [{'raumtyp': '', 'besucht': False}, {'raumtyp': 'S', 'besucht': True}],
+                    [{'raumtyp': 'L', 'besucht': True}, {'raumtyp': 'F', 'besucht': False}]
+                ],
+                {"x": 0, "y": 0},
+                [
+                    call('P', end = '  '), call('S', end = '  '),
+                    call(),
+                    call('L', end = '  '), call('?', end = '  '),
+                    call()
+                ]
+            ],
+            [
+                [
+                    [{'raumtyp': 'F', 'besucht': True}, {'raumtyp': 'S', 'besucht': True}],
+                    [{'raumtyp': '', 'besucht': False}, {'raumtyp': 'L', 'besucht': True}]
+                ],
+                {"x": 1, "y": 1},
+                [
+                    call('E', end = '  '), call('S', end = '  '),
+                    call(),
+                    call('?', end = '  '), call('P', end = '  '),
+                    call()
+                ]
+            ]
+        ]
 
-    @patch('builtins.print')
-    def test_position_player_korrekt(self, mock_print):
-        dungeon = generate_dungeon(2, 1)
-        print_dungeon(dungeon, (1, 0))
-        assert mock_print.mock_calls == [
-            call('E', end = '  '),
-            call(),
-            call('P', end = '  '),
+        for test in tests:
+            print_dungeon(test[0], test[1])
 
-            call()]
+            self.assertEqual(mock_print.mock_calls, test[2])
+            
+            mock_print.reset_mock()
